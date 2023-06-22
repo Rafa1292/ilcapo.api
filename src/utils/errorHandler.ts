6d2fc626bac6c953b6fd75291fee2028de5df311
Rafael Villalobos <rvilla3452@gmail.com>
2023-06-22 01:12:08 -0600

    varios

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,5 +1,12 @@
 
 export const errorHandler = (error: any): any => {
+  if (
+    error.name === 'UnauthorizedError' &&
+    error.inner.name === 'TokenExpiredError'
+  ) {
+    return ['Token expired']
+  }
+
   if (error?.errors?.length > 0) {
     const errors: string[] = error.errors.map((err: any) => {
       return err.message
