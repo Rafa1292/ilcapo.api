788ea0ee21c01e63a34bbb363ac2118ce485c72c
rafa1292 <rvilla3452@gmail.com>
2023-08-19 09:32:59 -0600

    timemanagerfix

--- src/utils/timeManager.ts
+++ src/utils/timeManager.ts
@@ -1,5 +1,5 @@
 export const getNow = (): Date => {
     const serverTimezone = 'America/Costa_Rica'
-    const now = new Date().toLocaleString('es-ES', { timeZone: serverTimezone })
+    const now = new Date().toLocaleString('en-US', { timeZone: serverTimezone })
     return new Date(now)
 }
